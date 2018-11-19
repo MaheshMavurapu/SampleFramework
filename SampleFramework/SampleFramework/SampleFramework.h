99cfc3782b239322fe516b5cd073a7d85c3e7dbe
Mahesh Mavurapu <mahesh.m@techjini.com>
2018-11-19 12:42:49 +0530

    Finished Sample Framework

---
+++ SampleFramework/SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Mahesh Mavurapu on 19/11/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
---
+++ SampleFramework/SampleFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  SampleFramework
+//
+//  Created by Mahesh Mavurapu on 19/11/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import Foundation
+
+public class Serice {
+    
+    private init() {
+        
+    }
+    
+    public static func doSomething() -> String {
+        return "Did something fishy!"
+    }
+}
